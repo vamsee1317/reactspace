91b85e8a10d227a6c065a7b2465887eccd61cbe2
Vamsee1317 <vamsee.achieversit@gmail.com>
2025-11-18 12:42:29 +0530

    ReactProjectSetUpInstallation

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactSetupGuide from './components/concepts/introduction/ReactSetupGuide'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <ReactSetupGuide />
+    </div>
+  )
+}
