67284b63d015d832f1a8bdce9123bf3332aab14b
Vamsee1317 <vamsee.achieversit@gmail.com>
2025-11-20 13:22:00 +0530

    React State Guide | App.jsx updated with styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import ReactSetupGuide from './components/concepts/introduction/ReactSetupGuide'
-import './App.css'
 import ReactComponentsGuide from './components/concepts/compTypes/ReactComponentsGuide'
 import ReactPropsGuide from './components/concepts/props/ReactPropsGuide'
+import ReactStateGuide from './components/concepts/states/ReactStateGuide'
+import './App.css'
 
 export default function App() {
   return (
-    <div>
-      <ReactSetupGuide />
-      <ReactComponentsGuide />
-      <ReactPropsGuide />
+    <div className="app-container">
+      
+      <div className="component-block">
+        <ReactSetupGuide />
+      </div>
+
+      <div className="component-block">
+        <ReactComponentsGuide />
+      </div>
+
+      <div className="component-block">
+        <ReactPropsGuide />
+      </div>
+
+      <div className="component-block">
+        <ReactStateGuide />
+      </div>
+
     </div>
   )
 }
