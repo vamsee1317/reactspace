01b90024cadfce7071fb21f068a609ac94735bc0
Vamsee1317 <vamsee.achieversit@gmail.com>
2025-11-20 12:48:08 +0530

    React Properties

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactSetupGuide from './components/concepts/introduction/ReactSetupGuide'
 import './App.css'
 import ReactComponentsGuide from './components/concepts/compTypes/ReactComponentsGuide'
+import ReactPropsGuide from './components/concepts/props/ReactPropsGuide'
 
 export default function App() {
   return (
     <div>
       <ReactSetupGuide />
       <ReactComponentsGuide />
+      <ReactPropsGuide />
     </div>
   )
 }
