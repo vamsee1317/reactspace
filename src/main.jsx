687d1bdc5e2e7b5fabbfd323181b10b476798e0f
Vamsee1317 <vamsee.achieversit@gmail.com>
2025-11-22 15:40:44 +0530

    Add theme support with light and dark mode styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
+import "./styles/theme.css";
 
 createRoot(document.getElementById('root')).render(
 
